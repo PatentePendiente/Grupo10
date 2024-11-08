cebf680d62e9e6b7a12796fc0b59548db08dee0f
juandiegomoscoso <programming.uxh5y@simplelogin.co>
2024-11-08 12:33:02 -0300

    Agregué creación del usuario para el supervisor

--- Com5600G10/03 Seguridad, Encriptacion y Backup - Scripts Entrega 5.sql
+++ Com5600G10/03 Seguridad, Encriptacion y Backup - Scripts Entrega 5.sql
@@ -12,6 +12,7 @@ INDICE:
 1) Creacion de Certificados y Claves para Encriptación
 2) Creacion de usuario y sede de permisos para el responsable de importar archivos
 3) Creacion del login para el supervisor
+4) Creacion del usuario para el supervisor
 
 */
 
@@ -70,6 +71,14 @@ BEGIN
     CREATE LOGIN SupervisorLogin WITH PASSWORD = 'Supervisor123!';
     PRINT 'Login SupervisorLogin creado.';
 END
+
+-- 4) Creacion del usuario para el supervisor
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'SupervisorUser')
+BEGIN
+    CREATE USER SupervisorUser FOR LOGIN SupervisorLogin;
+    PRINT 'Usuario SupervisorUser creado.';
+END
+
 
 
 
