d0b2c20d3e1a49c939460e0f6e012003e117c763
juandiegomoscoso <programming.uxh5y@simplelogin.co>
2024-11-10 14:25:16 -0300

    Moví la creación de certificados y claves para la encriptación de el archivo de Seguridad y Backup al archivo de Encriptación

--- Com5600G10/05 Encriptacion - Scripts Entrega 5.sql
+++ Com5600G10/05 Encriptacion - Scripts Entrega 5.sql
@@ -14,7 +14,31 @@ INDICE:
 
 */
 
+-- 1) Creación de certificados y claves para encriptación
+IF NOT EXISTS (SELECT * FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
+BEGIN
+    -- Para encriptar la tabla de empleados, decidimos usar el algorítmo AES_256 de encriptación simétrica, 
+	-- ya que es la más segura entre los algoritmos AES_128, AES_192, y AES_256
 
+	-- Creamos una clave maestra para proteger los certificados y claves que creemos a nivel base de datos
+	CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'ClaveEncriptacionEmpleados123';
+
+	-- Creamos el certificado que va a proteger la clave simétrica
+	CREATE CERTIFICATE EmpleadosCert
+	WITH SUBJECT = 'Certificado para encriptar los datos de empleados';
+
+	-- Creamos la clave simetrica que vamos a usar para encriptar la tabla de empleados 
+	CREATE SYMMETRIC KEY EmpleadosClaveSimetrica
+	WITH ALGORITHM = AES_256
+	ENCRYPTION BY CERTIFICATE EmpleadosCert;
+
+	PRINT 'Certificados y claves para encriptación creados';
+END
+ELSE
+    PRINT 'La clave simétrica EmpleadosClaveSimetrica ya existe';	
+GO
+
+-- 2) Creacion de la tabla EmpleadoEncriptado
 -- Debido a los datos privados de los empleados deben estar encritados,
 -- creamos una nueva tabla que va a almacenar los datos encriptados de los empledos
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[HR].[EmpleadoEncriptado]') AND type in (N'U'))
--- Com5600G10/07 Seguridad y Backup - Scripts Entrega 5.sql
+++ Com5600G10/07 Seguridad y Backup - Scripts Entrega 5.sql
@@ -9,7 +9,6 @@ Grupo 10 sqLite, Integrantes:
 
 
 INDICE: 
-1) Creacion de Certificados y Claves para Encriptación
 2) Creacion de usuario y sede de permisos para el responsable de importar archivos
 3) Creacion del login para el supervisor
 4) Creacion del usuario para el supervisor
@@ -18,31 +17,6 @@ INDICE:
 7) Conceder permiso de ejecución del SP crearNotaCredito al rol de Supervisor
 
 */
-
--- Modulo de creacion de certificados y claves:
--- 1) Certificados y Claves para Encriptación
-IF NOT EXISTS (SELECT * FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
-BEGIN
-    -- Para encriptar la tabla de empleados, decidimos usar el algorítmo AES_256 de encriptación simétrica, 
-	-- ya que es la más segura entre los algoritmos AES_128, AES_192, y AES_256
-
-	-- Creamos una clave maestra para proteger los certificados y claves que creemos a nivel base de datos
-	CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'ClaveEncriptacionEmpleados123';
-
-	-- Creamos el certificado que va a proteger la clave simétrica
-	CREATE CERTIFICATE EmpleadosCert
-	WITH SUBJECT = 'Certificado para encriptar los datos de empleados';
-
-	-- Creamos la clave simetrica que vamos a usar para encriptar la tabla de empleados 
-	CREATE SYMMETRIC KEY EmpleadosClaveSimetrica
-	WITH ALGORITHM = AES_256
-	ENCRYPTION BY CERTIFICATE EmpleadosCert;
-
-	PRINT 'Certificados y claves para encriptación creados';
-END
-ELSE
-    PRINT 'La clave simétrica EmpleadosClaveSimetrica ya existe';	
-GO
 
 --2) Creacion de usuario y sede de permisos para el responsable de importar archivos
 -- Verificar si el LOGIN ya existe
