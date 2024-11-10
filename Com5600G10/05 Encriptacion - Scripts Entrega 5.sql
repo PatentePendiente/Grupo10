51c096221d2ded17ea293b9c667d1f388f9ed04e
juandiegomoscoso <programming.uxh5y@simplelogin.co>
2024-11-10 16:04:32 -0300

    Agregué creación de certificados y claves para la encriptación

--- Com5600G10/05 Encriptacion - Scripts Entrega 5.sql
+++ Com5600G10/05 Encriptacion - Scripts Entrega 5.sql
@@ -9,89 +9,37 @@ Grupo 10 sqLite, Integrantes:
 
 
 INDICE: 
+1) Creaci贸n de certificados y claves para encriptaci贸n
+2) Creacion de la tabla EmpleadoEncriptado
+3) Creacion de un stored procedure para importar los datos de empleados encriptados
 
 
 */
 
+-- 1) Creaci贸n de certificados y claves para encriptaci贸n
+IF NOT EXISTS (SELECT * FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
+BEGIN
+    -- Para encriptar la tabla de empleados, decidimos usar el algor铆tmo AES_256 de encriptaci贸n sim茅trica, 
+	-- ya que es la m谩s segura entre los algoritmos AES_128, AES_192, y AES_256
 
+	-- Creamos una clave maestra para proteger los certificados y claves que creemos a nivel base de datos
+	CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'ClaveEncriptacionEmpleados123';
 
+	-- Creamos el certificado que va a proteger la clave sim茅trica
+	CREATE CERTIFICATE EmpleadosCert
+	WITH SUBJECT = 'Certificado para encriptar los datos de empleados';
 
+	-- Creamos la clave simetrica que vamos a usar para encriptar la tabla de empleados 
+	CREATE SYMMETRIC KEY EmpleadosClaveSimetrica
+	WITH ALGORITHM = AES_256
+	ENCRYPTION BY CERTIFICATE EmpleadosCert;
 
+	PRINT 'Certificados y claves para encriptaci贸n creados';
+END
+ELSE
+    PRINT 'La clave sim茅trica EmpleadosClaveSimetrica ya existe';	
+GO
 
-
--- MODIFICACION PARA LA ENTREGA 5
--- Debido a que la tabla Empleado va a almacenar datos encritados,
--- cambiamos los tipos de datos a VARBINARY para que continen datos sensibles
-/*
-	CREATE TABLE HR.Empleado (
-		legajo INT PRIMARY KEY, -- No encriptamos el legajo porque es una clave primaria
-		nombre VARBINARY(8000),  
-		apellido VARBINARY(8000),  
-		dni VARBINARY(8000),  
-		direccion VARBINARY(8000),  
-		cargo VARBINARY(8000),  
-		turno VARBINARY(8000),  --No deberia estar encriptado porej
-		idSuc TINYINT NOT NULL, -- No encriptamos el id de la sucursal porque es una clave for醤ea
-		mailPersonal VARBINARY(8000),  
-		mailEmpresa VARBINARY(8000),  
-		fechaBorrado DATE NULL,
-
-    	CONSTRAINT fkSucursal FOREIGN KEY (idSuc) REFERENCES hr.sucursal(nroSucursal)
-	);
-	*/
-
-
-	/*
-
-	-- MODIFICACION PARA LA ENTREGA 5
-	-- Se nos pide encriptar la tabla de empleados porque contiene informacion privada.
-	-- Debido a que la version de SQL SERVER que utilizamos para dise馻r la base de datos (SQL SERVER EXPRESS)
-	-- no admite la encriptaci髇 por tabla, tenemos que hacerla por columnas.
-	-- Debemos encriptar cada valor antes de insertarlo en la tabla Empleado.
-
-	-- Abrimos la clave simetrica para usarla para la encriptaci髇 
-	OPEN SYMMETRIC KEY EmpleadosClaveSimetrica
-	DECRYPTION BY CERTIFICATE EmpleadosCert;
-
-	-- Insertamos los datos encriptado en la tabla
-	MERGE INTO HR.Empleado AS TARGET
-	USING (
-	 SELECT 
-		CAST(t.legajo AS INT) AS legajo, -- No encriptamos el legajo ya que es la clave primaria de la tabla
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.nombre) AS nombre, 
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.apellido) AS apellido, 
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), CAST(CAST(t.doc AS INT) AS CHAR(8))) AS dni, 
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.direccion) AS direccion, 
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.cargo) AS cargo, 
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.turno) AS turno, 
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.emailPers) AS emailPers,
-		ENCRYPTBYKEY(KEY_GUID('EmpleadosClaveSimetrica'), t.emailEmp) AS emailEmp, 
-		S.nroSucursal AS nroSucursal -- No encriptamos el nro de sucursal ya que es clave foranea
-	FROM #tablaImportada t
-	INNER JOIN HR.Sucursal s on t.surcursal = s.localidad
-	) AS SOURCE
-	ON TARGET.legajo = SOURCE.legajo
-	WHEN MATCHED THEN 
-	  UPDATE SET 
-        TARGET.nombre = SOURCE.nombre,
-        TARGET.apellido = SOURCE.apellido,
-        TARGET.dni = SOURCE.dni,
-        TARGET.direccion = SOURCE.direccion,
-        TARGET.cargo = SOURCE.cargo,
-        TARGET.turno = SOURCE.turno,
-        TARGET.mailPersonal = SOURCE.emailPers,
-        TARGET.mailEmpresa = SOURCE.emailEmp,
-        TARGET.idSuc = SOURCE.nroSucursal
-	WHEN NOT MATCHED THEN 
-    INSERT (legajo, nombre, apellido, dni, direccion, cargo, turno, mailPersonal, mailEmpresa, idSuc)
-    VALUES (SOURCE.legajo, SOURCE.nombre, SOURCE.apellido, SOURCE.dni, SOURCE.direccion, SOURCE.cargo, SOURCE.turno, SOURCE.emailPers, SOURCE.emailEmp, SOURCE.nroSucursal);
-
-	-- Cerramos la clave simetrica
-	CLOSE SYMMETRIC KEY EmpleadosClaveSimetrica;
-	
-
-
-	*/
 
 
 
